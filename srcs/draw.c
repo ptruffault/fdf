978dc025a823d46c975c0ee94b49cfdd75fc8d9c
Paul Truffault <ptruffau@student.42.fr>
2020-01-29 14:15:14 +0100

    make save

--- srcs/draw.c
+++ srcs/draw.c
@@ -31,12 +31,13 @@ void	draw_map_pts(t_windows *window, t_map *data)
 {
 	int i;
 	int j;
+	t_point a;
+	t_point b;
 	int x1;
 	int y1;
 	int x2;
 	int y2;
 
-    (void)window;
 	i = -1;
 	window->color = 0xDC143C;
 	while (++i < data->height)
@@ -44,27 +45,27 @@ void	draw_map_pts(t_windows *window, t_map *data)
 		j = -1;
 		while (++j < data->width)
 		{
-			x1 = data->margin_left + j  * data->dist_pts_x;
-			y1 = data->margin_up + i * data->dist_pts_y + (data->angle_y == 0 ? 0 : data->points[i][j].z * data->z_multiplicateur);
-			
+			a.x = data->margin_left + j  * data->dist_pts_x;
+			a.y = data->margin_up + i * data->dist_pts_y + (data->angle_y == 0 ? 0 : data->points[i][j].z * data->z_multiplicateur);
+			a.color = data->points[i][j].color;
+
 			// right line
 			if (j != data->width - 1)
 			{
 				// right
-				x2 = data->margin_left + (j + 1) * data->dist_pts_x;
-				y2 = data->margin_up + i * data->dist_pts_y + (data->angle_y == 0 ? 0 : data->points[i][j + 1].z * data->z_multiplicateur);
-				init_brensenham(x1, y1, x2, y2, window);
-				
-				
+				b.x = data->margin_left + (j + 1) * data->dist_pts_x;
+				b.y = data->margin_up + i * data->dist_pts_y + (data->angle_y == 0 ? 0 : data->points[i][j + 1].z * data->z_multiplicateur);
+				b.color = data->points[i][j + 1].color;
+				init_brensenham(&a, &b, window);
 			}
-			
 
 			// down line
 			if (i != data->height - 1)
 			{
-				x2 = data->margin_left + j * data->dist_pts_x;
-				y2 = data->margin_up + (i + 1) * data->dist_pts_y + (data->angle_y == 0 ? 0 : data->points[i + 1][j].z * data->z_multiplicateur);
-				init_brensenham(x1, y1, x2, y2, window);
+				b.x = data->margin_left + j * data->dist_pts_x;
+				b.y = data->margin_up + (i + 1) * data->dist_pts_y + (data->angle_y == 0 ? 0 : data->points[i + 1][j].z * data->z_multiplicateur);
+				b.color = data->points[i + 1][j].color;
+				init_brensenham(&a, &b, window);
 			}
 
 		}
